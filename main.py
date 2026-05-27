18c1449dfeddd7534cea0a2fc7e524396e76ab04
Gaby <gbenincasa042@gmail.com>
2026-05-27 14:58:51 -0300

    Realizamos una API REST con una base de datos falsa en memoria sobre cursos de programacion con FastAPI en Python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # API REST: interfaz de programacion de aplicaciones para compartir recursos
 
+from operator import index
 from typing import List, Optional
 import uuid
 from fastapi import FastAPI, HTTPException
@@ -10,7 +11,7 @@ app = FastAPI()
 
 #Aca definimos el modelo
 class Curso(BaseModel):
-    id: str
+    id: Optional[str] = None
     nombre: str
     descripcion: Optional[str] = None 
     nivel: str
@@ -30,6 +31,7 @@ def obtener_cursos():
 def crear_curso(curso: Curso):
     curso.id = str(uuid.uuid4()) # Usamos uuid para generar un id unico e irrepetible
     cursos_db.append(curso) 
+    return curso
 
 # CRUD: Read(Lectura) GET(individual): Leeremos el curso que coincida con el ID que pidamos
 @app.get("/cursos/{curso_id}", response_model=Curso)
@@ -41,9 +43,21 @@ def obtener_curso(curso_id: str):
 
 # CRUD: Update (Actualizar/Modificar) PUT: modificaremos un recurso que coincida con el ID que mandemos
 @app.put("/cursos/{curso_id}", response_model= Curso)
-def modificar_curso(curso_id: str):
+def modificar_curso(curso_id: str, curso_actualizado: Curso):
     curso = next((curso for curso in cursos_db if curso.id == curso_id) , None)
     if curso is None:
         raise HTTPException(status_code=404, detail = "curso no encontrado")
-    return curso
+    curso_actualizado.id = curso_id
+    index = cursos_db.index(curso)
+    cursos_db[index] = curso_actualizado
+    return curso_actualizado
 
+#CRUD: Delete (Eliminar/Baja)  DELETE: Eliminaremos un recurso que coincida con el ID que mandemos
+@app.delete("/cursos/{curso_id}", response_model=Curso)
+def eliminar_curso(curso_id: str,):
+    curso = next((curso for curso in cursos_db if curso.id == curso_id), None) # Con next tomamos la primera coincidencia del array devuelto
+    if curso is None:
+        raise HTTPException(status_code=404, detail = "Curso no encontrado")
+    cursos_db.remove(curso)
+
+    return curso
